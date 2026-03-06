72ff32fe5f63b53b295015542c77b7de1ca83113
xiujun58 <39918118+xiujun58@users.noreply.github.com>
2026-03-06 17:34:46 +0800

    Refactor config keys to camelCase format

--- src/config.js
+++ src/config.js
@@ -21,11 +21,11 @@
 // };
 
 module.exports = {
-    TUSHARE_TOKEN: "5cb81e2f3e7ffaaf33da77c4b451ea1466732846f79ac27dc86fd621",
-    DEEPSEEK_API_KEY: "sk-1370abd114e8493caa1333369ce0ae82",
-    DEEPSEEK_BASE_URL: "https://api.deepseek.com",
-    DEEPSEEK_MODEL: "deepseek-chat",
-    TELEGRAM_BOT_TOKEN: "8595166028:AAFM6aqqCmDVczA1RrqdaFhWvl-_UON-p4U",
-    TELEGRAM_ALLOWED_BOT_ID: "5908447022",
-    TELEGRAM_ALLOWED_BOT_USERNAME: "@xj_get_rich_bot",
+    tushareToken: "5cb81e2f3e7ffaaf33da77c4b451ea1466732846f79ac27dc86fd621",
+    deepseekApiKey: "sk-1370abd114e8493caa1333369ce0ae82",
+    deepseekBaseUrl: "https://api.deepseek.com",
+    deepseekModel: "deepseek-chat",
+    telegramBotToken: "8595166028:AAFM6aqqCmDVczA1RrqdaFhWvl-_UON-p4U",
+    telegramAllowedBotId: "5908447022",
+    telegramAllowedBotUsername: "@xj_get_rich_bot",
 }
