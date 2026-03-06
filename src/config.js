4f9603c6201663420a7f6276d5e8fa7f8d7fa5af
xiujun58 <39918118+xiujun58@users.noreply.github.com>
2026-03-06 17:44:09 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,31 +1,23 @@
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
-// dotenv.config();
+dotenv.config();
 
-// function getEnv(name, required = true) {
-//   const value = process.env[name];
-//   if (required && !value) {
-//     throw new Error(`缺少环境变量: ${name}`);
-//   }
-//   return value;
-// }
-
-// module.exports = {
-//   tushareToken: getEnv('TUSHARE_TOKEN'),
-//   deepseekApiKey: getEnv('DEEPSEEK_API_KEY'),
-//   deepseekBaseUrl: process.env.DEEPSEEK_BASE_URL || 'https://api.deepseek.com',
-//   deepseekModel: process.env.DEEPSEEK_MODEL || 'deepseek-chat',
-//   telegramBotToken: getEnv('TELEGRAM_BOT_TOKEN', false),
-//   telegramAllowedBotId: process.env.TELEGRAM_ALLOWED_BOT_ID,
-//   telegramAllowedBotUsername: process.env.TELEGRAM_ALLOWED_BOT_USERNAME
-// };
+function getEnv(name, required = true) {
+  const value = process.env[name];
+  if (required && !value) {
+    throw new Error(`缺少环境变量: ${name}`);
+  }
+  return value;
+}
 
 module.exports = {
-    tushareToken: "5cb81e2f3e7ffaaf33da77c4b451ea1466732846f79ac27dc86fd621",
-    deepseekApiKey: "sk-1370abd114e8493caa1333369ce0ae82",
-    deepseekBaseUrl: "https://api.deepseek.com",
-    deepseekModel: "deepseek-chat",
-    telegramBotToken: "8595166028:AAFM6aqqCmDVczA1RrqdaFhWvl-_UON-p4U",
-    telegramAllowedBotId: "5908447022",
-    telegramAllowedBotUsername: "@xj_get_rich_bot",
-}
+    tushareToken: getEnv('TUSHARE_TOKEN'),
+    deepseekApiKey: getEnv('DEEPSEEK_API_KEY'),
+    deepseekBaseUrl: getEnv('DEEPSEEK_BASE_URL'),
+    deepseekModel: getEnv('DEEPSEEK_MODEL'),
+    telegramBotToken: getEnv('TELEGRAM_BOT_TOKEN', false),
+    telegramAllowedBotId: getEnv('TELEGRAM_ALLOWED_BOT_ID'),
+    telegramAllowedBotUsername: getEnv('TELEGRAM_ALLOWED_BOT_USERNAME'),
+};
+
+
